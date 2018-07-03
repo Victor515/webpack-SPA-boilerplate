517e68513f89d5b3bce30410cd57f72bc716cab6
victor515 <victorlevian@gmail.com>
2018-07-03 14:13:18 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    devtool: 'eval-source-map',
+    entry:  __dirname + "/app/main.js",// the entry point
+    output: {
+      path: __dirname + "/public", // the path to store bundled file
+      filename: "bundle.js" // the name of bundled files
+    },
+
+    devServer:{
+      contentBase: "./public",
+      historyApiFallback: true,
+      inline: true // real-time refresh
+    },
+
+    module: {
+      rules: [
+          {
+              test: /(\.jsx|\.js)$/,
+              use: {
+                  loader: "babel-loader"
+              },
+              exclude: /node_modules/
+          },
+          {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader",
+                    options: {
+                      modules: true, // enable css modules
+                      localIdentName: '[name]__[local]--[hash:base64:5]' // css class names
+                  }
+                }
+            ]
+          }
+      ]
+    }
+  }
